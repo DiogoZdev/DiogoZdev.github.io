853fb8c1009c1d09c8dd285e5acdf67399a27a8b
DiogoZ <diogozang2@gmail.com>
2021-06-20 23:59:44 -0300

    atualização de layout e inserção do C4E

---
+++ scripts/screenFunctions.js
@@ -0,0 +1,12 @@
+function change_color(el) {
+    
+    el.style.color = "orange"
+}
+
+function showContactInfo() {
+    document.querySelector('#contactInfo').style.marginRight = 0;
+}
+
+function hideContactInfo() {
+    document.querySelector('#contactInfo').style.marginRight = "-400px";
+}
