93ecc8532a83ee4108e201033474843ca2eb3846
DiogoZ <diogozang2@gmail.com>
2021-06-22 22:16:39 -0300

    organization and responsive main layout

--- scripts/screenFunctions.js
+++ scripts/screenFunctions.js
@@ -1,6 +1,6 @@
 function change_color(el) {
     
-    el.style.color = "orange"
+    el.style.color = "#FFA500";
 }
 
 function showContactInfo() {
@@ -8,5 +8,15 @@ function showContactInfo() {
 }
 
 function hideContactInfo() {
-    document.querySelector('#contactInfo').style.marginRight = "-400px";
+    document.querySelector('#contactInfo').style.marginRight = "-100%";
+}
+
+function color(elm) {
+
+    var color = Math.floor(Math.random()*16).toString(16);
+
+    var randomColor = '#'+color+color+color;
+
+    elm.style.backgroundColor = randomColor;
+
 }
