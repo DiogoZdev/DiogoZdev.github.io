87710dd655a816f2611884185176d52d757b5c39
DiogoZ <diogozang2@gmail.com>
2021-07-17 22:49:51 -0300

    mobile menu and icons updated

--- scripts/screenFunctions.js
+++ scripts/screenFunctions.js
@@ -1,22 +1,3 @@
-function change_color(el) {
-    
-    el.style.color = "#FFA500";
-}
-
-function showContactInfo() {
-    document.querySelector('#contactInfo').style.marginRight = 0;
-}
-
-function hideContactInfo() {
-    document.querySelector('#contactInfo').style.marginRight = "-100%";
-}
-
-function color(elm) {
-
-    var color = Math.floor(Math.random()*16).toString(16);
-
-    var randomColor = '#'+color+color+color;
-
-    elm.style.backgroundColor = randomColor;
-
+function toggle_mobile_menu() {
+    document.querySelector('.mobile-menu').classList.toggle('hidden');
 }
